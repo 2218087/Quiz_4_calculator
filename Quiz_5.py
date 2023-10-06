5b06503a667f382d6e1f5c6a2b16bcc2ae7334f1
CHOI HAN BI <144421288+2218087@users.noreply.github.com>
2023-10-06 16:46:21 +0900

    Add files via upload

--- Quiz_5.py
+++ Quiz_5.py
@@ -4,7 +4,7 @@ print("구구단을 출력합니다")
 def number(var):
     for x in range(1, var+1):
         print("-----[" + str(x) + "단]-----")
-        for y in range(1, 10):
+        for y in range(1, var+1):
            print(x, "x", y, "=", x*y)
 
 number(var)
